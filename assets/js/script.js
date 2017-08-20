3d0e9e23c16096376757b2bc5bc75ac29d2811c8
Daniel Muller <DMuller@DM.local>
2017-08-20 15:00:38 -0400

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+
+
+var carStart = new Audio("assets/carstart.wav");
+
+setTimeout(playLogo, 1000*3);
+
+
+function playLogo(){
+
+	$("#logoDiv").append("<h1>Trivia!</h1>").fadeIn(3000);
+	//need trivia to just show up after like 2 seconds
+	//play music here too maybe
+	console.log("trivia after 5 seconds works!")
+	carStart.play();
+
+}
+
+
+function startButton() {
+
+	//start button shows up, and when clicked, to start game
+
+function playGame() {
+	//load a question
+		//start timer
+		//on question answer or timer up, go to next question
+}
+
+}
+
+function endGame() {
+
+	//tally up the scores and such, display
+}
+
+
+
+
+
+
+
+
+// Marty meets Doc at which shopping mall in the first Back to the Future film?
+// Lone Pine, Wild Oak, Big Fir, *Twin Pines
+
+// What is printed on the license plate of the time-traveling DeLorean?
+// Science, TimeTvl, *OutaTime, Time4U
+
+// Who is playing in the World Series in 2015?
+// *Chicago Miami, NY LA, San Fran TX, Seattle Atlanta
+
+// What date do Marty, Doc, and Jennifer travel to in Back to the Future II?
+// 9/22/15, 11/8/15, 12/1/15, *10/21/15
+
+// What kind of drink does Marty ask for when he arrives in the old West?
+// Milk, *IceWater, Pepsi, Root Beer
+
+// After Marty arrives in 1855, the next train will be leaving town on Monday at what time?
+// 7a, *8a, 9a, 10a
+
+// What is Buford Tannen's nickname?
+// Lone Wolf, Crazy Snake, *Mad Dog, Wild Bull
+
+// Marty is to meet up with Griff Tannen in which cafe in 2015?
+// Cafe Future, *Cafe 80's, Giggawatts, Space Cafe
+
+// What does Biff run into after he chases Marty around the Hill Valley square?
+// Garbage, Dirt, *Manure, tar.
+
+// What does Doc Brown call his dog in 1955?
+// *Copernicus, Edison, Einstein, Tesla
+
+
+
